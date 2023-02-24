1b38c8f47869750c8e5ce9948348c023c2604a52
filipkokowski999 <filipkokowski999@gmail.com>
2023-02-24 15:56:23 +0100

    Added JSON-Simple lib

--- src/org/engine/BodyPartsHandler.java
+++ src/org/engine/BodyPartsHandler.java
@@ -1,14 +1,50 @@
 package org.engine;
 
-import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 
-import org.gameobjects.BodyParts;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class BodyPartsHandler {
-
+	
+	public BodyPartsHandler(String path) {
+		URL url = BodyPartsHandler.class.getResource(path);
+		String urlStr = url.getPath();
+		
+		try {
+			Object jsonFile = new JSONParser().parse(new FileReader(urlStr));
+			JSONObject jsonObject = (JSONObject) jsonFile;
+			
+			for(Object key : jsonObject.keySet()) {
+				JSONObject side = (JSONObject) jsonObject.get(key);
+				
+				for(Object part : side.keySet()) {
+					JSONObject bodyPart = (JSONObject) side.get(part);
+					
+					try{
+						for(Object partElement : bodyPart.keySet()) {
+							JSONObject element = (JSONObject) bodyPart.get(partElement);
+							
+							
+							
+							System.out.println(element);
+						}	
+					}
+					catch(ClassCastException e) {
+						System.out.println(bodyPart + "Dawdjahwvgdvawj");
+					}
+				}
+			}
+			
+		} 
+		catch (FileNotFoundException e) { e.printStackTrace(); } 
+		catch (IOException e) { e.printStackTrace(); } 
+		catch (ParseException e) { e.printStackTrace(); }	
+		
+	}
+	
 }
